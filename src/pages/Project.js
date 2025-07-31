39b36af8561765a18492adec9d88780d5a949d02
Pantira <pantira151@gmail.com>
2025-07-31 15:40:05 +0700

    closeButton แก้ไข

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -229,12 +229,7 @@ function Project() {
                 <div className={styles.menuOverlay} onClick={closeMobileMenu}></div>
                 <div className={styles.mobileMenu}>
                   <div className={styles.menuHeader}>
-                    <h3>Filters</h3>
                     <button className={styles.closeButton} onClick={closeMobileMenu}>
-                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                        <line x1="18" y1="6" x2="6" y2="18"></line>
-                        <line x1="6" y1="6" x2="18" y2="18"></line>
-                      </svg>
                     </button>
                   </div>
                   
